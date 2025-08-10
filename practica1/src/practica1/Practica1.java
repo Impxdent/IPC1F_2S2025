b813bb91a07f30da7c39c5f8fc4587fe2f2473e8
André Escobar <andreescobara@gmail.com>
2025-08-09 23:57:32 -0600

    Segundo commit ya con el menu de opciones

--- practica1/src/practica1/Practica1.java
+++ practica1/src/practica1/Practica1.java
@@ -14,7 +14,17 @@ public class Practica1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Elija su opción");
+        System.out.println("Agregar personaje:");
+        System.out.println("Modificar personaje");
+        System.out.println("Eliminar personaje");
+        System.out.println("Ver datos de los personajes");
+        System.out.println("Ver listado de personajes");
+        System.out.println("Realizar pelea entre personajes");
+        System.out.println("Ver historial de peleas");
+        System.out.println("Ver datos de estudiante");
+        System.out.println("Salir");
+        
     }
     
 }
